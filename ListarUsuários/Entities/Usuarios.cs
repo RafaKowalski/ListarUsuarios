3af06c0e26e9500ee70bf3a7ec2422c019220a66
Rafael Kowalski <rafaelmk06@gmail.com>
2022-04-28 22:52:21 -0300

    Adicionar arquivos de projeto.

---
+++ ListarUsuários/Entities/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿namespace ListarUsuários.Entities
+{
+    public class Usuarios
+    {
+        public string Name { get; set; }
+
+        public Usuarios(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return
+              "Nome: "
+             +Name;
+        }
+    }
+}
---
+++ ListarUsuários/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ListarUsuários.Entities;
+
+namespace ListarUsuario
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o numero de usuarios que deseja listar: ");
+            int users = int.Parse(Console.ReadLine());
+
+            List <Usuarios> list = new List<Usuarios>();
+
+            for (int i = 1; i <= users; i++)
+            {
+                Console.WriteLine($"usuário {i}");
+                Console.WriteLine("Nome");
+                string name = Console.ReadLine();
+
+                list.Add(new Usuarios(name));
+            }
+
+            Console.WriteLine("Lista de todos os usuário/melhor epoca do cav");
+            foreach (Usuarios usu in list)
+                Console.WriteLine(usu.ToString());
+        }
+    }
+}
